f0917bd553a9e891df1256a6bde8939b69ad030f
Henrik Lund Mortensen <HenrikLundMortensen@gmail.com>
2017-09-12 17:31:06 +0200

    Working determinant regression example

--- regressionExample.py
+++ regressionExample.py
@@ -1,17 +1,60 @@
-from sklearn.neural_network import MLPRegressor 
+import numpy as np
+from sklearn.neural_network import MLPRegressor
+from matplotlib import pyplot as plt
+import time
  
 # create Trainig Dataset
-train_x=[[x] for x in  range(200)]
-train_y=[x[0]**2 for x in train_x]
- 
+# X = np.linspace(-10,10,1000).reshape(-1,1)
+# y = np.array(np.sin(X) +np.sin(X*2)+np.sin(X*5)).reshape(-1,1)
+# y = np.ravel(y)
 #create neural net regressor
-reg = MLPRegressor(hidden_layer_sizes=(100,100),solver="lbfgs")
-reg.fit(train_x,train_y)
- 
+reg = MLPRegressor(hidden_layer_sizes=(10,10),solver="lbfgs",max_iter=10000,alpha = 0.001)
+# reg.fit(X,y)
+
+# fig = plt.figure()
+# ax = fig.gca()
+# ax.plot(X,y,color='red')
+# xtest = np.linspace(-10,10,2000).reshape(-1,1)
+# plt.plot(xtest,reg.predict(xtest),color='black')
+# fig.savefig('test.png')
+
+# plt.plot(X,reg.predict(X))
 #test prediction
-test_x=[[x] for x in  range(201,220,2)]
+
  
-predict=reg.predict(test_x)
-print("_Input_\t_output_")
-for i in range(len(test_x)):
-    print("  ",test_x[i],"---->",predict[i],"    should be",test_x[i][0]**2)
+# Create determinant testset
+
+MatList = []
+detList = []
+N = 3
+for i in range(10000):
+    MatList.append(np.random.rand(N**2))
+    detList.append(np.linalg.det(MatList[i].reshape(N,N)))
+    
+MatList = np.array(MatList)
+detList = np.array(detList)
+reg.fit(MatList,detList)
+
+
+Ntest = 100
+error = 0
+timedet = 0
+timepredict = 0
+
+for i in range(Ntest):
+    print(i)
+    
+    testmat = np.random.rand(N**2)
+    t1 = time.time()
+    testdet = np.linalg.det(testmat.reshape(N,N))
+    timedet += time.time() - t1
+
+    t1 = time.time()
+    predictdet = reg.predict([testmat])
+    timepredict += time.time() - t1
+    
+    error += (testdet-predictdet)**2
+
+print('Error = %g' %(error/Ntest))
+print(timedet)
+print(timepredict)
